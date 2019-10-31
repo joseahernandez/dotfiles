e4d33ce4b381ebe045ef89cca9b62fc38d8bb1c9
Jose Hernández <josehernandz@gmail.com>
2019-10-31 15:12:24 +0100

    Delete unused code from key bindings

--- terminal/zsh/key-bindings.zsh
+++ terminal/zsh/key-bindings.zsh
@@ -1,11 +1,5 @@
-## For Ctrl + R search
-__fzf_use_tmux__() {
-  [ -n "$TMUX_PANE" ] && [ "${FZF_TMUX:-0}" != 0 ] && [ ${LINES:-40} -gt 15 ]
-}
-
 __fzfcmd() {
-  __fzf_use_tmux__ &&
-    echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
+    echo "fzf"
 }
 
 # CTRL-R - Paste the selected command from history into the command line
