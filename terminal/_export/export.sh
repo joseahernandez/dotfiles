8f91cd9f7515935f7aaaa51b007669746dd2bb6b
Jose Hernandez <josehernandz@gmail.com>
2019-12-04 17:45:43 +0100

    Fix PATH variable

--- terminal/_export/export.sh
+++ terminal/_export/export.sh
@@ -4,9 +4,8 @@ export JAVA_HOME=$JAVA_8_HOME
 
 export FZF_DEFAULT_OPTS="--multi --layout=reverse"
 
-PATH=${DOTFILES_PATH}/scripts:$PATH
+PATH=${DOTFILES_PATH}/bin:$PATH
 PATH=${JAVA_HOME}/bin:$PATH
-PATH=${DOTFILES_PATH}/scripts:$PATH
 PATH=/sbin:$PATH
 PATH=/usr/sbin:$PATH
 PATH=/usr/local/sbin:$PATH
