f5630a498142df92e87397b510fa85ff192a5d06
Jose Hernandez <josehernandz@gmail.com>
2020-06-23 12:18:36 +0200

    Add some utilities scripts

--- terminal/_export/export.sh
+++ terminal/_export/export.sh
@@ -4,6 +4,7 @@ export JAVA_HOME=$JAVA_8_HOME
 
 export FZF_DEFAULT_OPTS="--multi --layout=reverse"
 
+export HOMEBREW_BUNDLE_FILE_PATH="${DOTFILES_PATH}/macOS/brew/Brewfile"
 PATH=${DOTFILES_PATH}/bin:$PATH
 PATH=${JAVA_HOME}/bin:$PATH
 PATH=/sbin:$PATH
