59264cce6866328c990ed2d19fe39e786f01c58f
Jose Hernández <josehernandz@gmail.com>
2022-10-07 22:09:14 +0200

    Update export file

--- terminal/_export/export.sh
+++ terminal/_export/export.sh
@@ -1,10 +1,14 @@
-export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
 export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
-export JAVA_HOME=$JAVA_8_HOME
+export JAVA_16_HOME=$(/usr/libexec/java_home -v16)
+export JAVA_HOME=$JAVA_16_HOME
 
 export FZF_DEFAULT_OPTS="--multi --layout=reverse"
 
 export HOMEBREW_BUNDLE_FILE_PATH="${DOTFILES_PATH}/macOS/brew/Brewfile"
+
+export NVM_DIR="$HOME/.nvm"
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/usr/local/opt/openssl@1.1"
+
 PATH=${DOTFILES_PATH}/bin:$PATH
 PATH=${JAVA_HOME}/bin:$PATH
 PATH=/sbin:$PATH
