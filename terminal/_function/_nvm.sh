6e261da5a5d81ededddfa3634b195abf81c5ca70
Jose Hernández <3692302+joseahernandez@users.noreply.github.com>
2024-03-21 17:16:11 +0100

    Update load nvm

--- terminal/_function/_nvm.sh
+++ terminal/_function/_nvm.sh
@@ -2,8 +2,8 @@
 # Lazy load of nvm the function is called equals the command
 function nvm() {
   unset -f nvm # The first time that function is called, we remove it and now the nvm command is used instead the function
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+    [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
   nvm "$@"
 }
